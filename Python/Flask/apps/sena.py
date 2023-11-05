a094b7f745a589c8f8ca1916326c1d787a7170e3
Manuel Caro <manuelcaro321@hotmail.com>
2023-11-05 01:40:53 -0500

    refactor set db with routes and running app

--- Python/Flask/apps/sena.py
+++ Python/Flask/apps/sena.py
@@ -1,9 +1,9 @@
 from flask import Flask, Config
 from utils import use_env, once_callable
-from apps.sena import use_app
 from services.db.mysql import create_connection
-from services.apps.sena import AppDB
-from routes.sena import AppRoutes
+from services.apps.sena import create_app_db
+from routes.sena import create_routes
+from run import run_app
 
 APP_NAME = "SENA"
 use_env = use_env("APP_" + APP_NAME)
@@ -11,7 +11,6 @@ use_env = use_env("APP_" + APP_NAME)
 @once_callable
 def use_config(root_path: str):
   env = use_env()
-  
   config_default = {
     **env
   }
@@ -30,16 +29,45 @@ def use_app():
 @once_callable
 def use_mysqldb():
   app = use_app()
-  mysql = create_connection(app)
-  return mysql
+  mysql_connection = create_connection(app)
+  return mysql_connection
 
 @once_callable
 def use_app_db():
   connection = use_mysqldb()
-  appDB = AppDB(connection)
+  appDB = create_app_db(connection)
   return appDB
 
 @once_callable
 def use_app_routes():
   app = use_app()
   appDB = use_app_db()
+  appRoutes = create_routes(app, appDB)
+  return appRoutes
+
+@once_callable
+def use_app_route_aprendiz():
+  aprendiz = use_app_routes().use_aprendiz()
+  aprendiz.setAllRoutes()
+  return aprendiz
+
+@once_callable
+def use_app_route_programa():
+  programa = use_app_routes().use_programa()
+  programa.setAllRoutes()
+  return programa
+
+@once_callable
+def use_run_app():
+  app = use_app()
+  use_app_db()
+  use_app_routes()
+  
+  use_app_route_aprendiz()
+  use_app_route_programa()
+  
+  run_app(app)
+  return app
+
+if __name__ == '__main__':
+  use_run_app()
