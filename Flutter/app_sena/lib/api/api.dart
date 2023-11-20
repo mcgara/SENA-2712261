a10872cba78b89d565158720c6388c53f8c54b02
Manuel Caro <manuelcaro321@hotmail.com>
2023-11-20 16:32:11 -0500

    add routes and fix dotenv

--- Flutter/app_sena/lib/api/api.dart
+++ Flutter/app_sena/lib/api/api.dart
@@ -1,30 +1,46 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import './router.dart' show Router;
+
+class DefaultApi {
+  static const host = 'localhost';
+  static const port = '8041';
+  static const url = 'http://$host:$port';
+  static const https = false;
+} 
 
 class ApiEnv {
-  static get host {
-    String host = dotenv.env['API_HOST'] ?? 'localhost';
-    return host;
+  static String get host {
+    return dotenv.env['API_HOST'] ?? DefaultApi.host;
   }
-  static get port {
-    int port = 8051;
+
+  static int get port {
+    int port = int.parse(DefaultApi.port);
     try {
-      port = int.parse(dotenv.env['API_PORT'] ?? '8051');
+      port = int.parse(dotenv.env['API_PORT'] ?? '::not port::');
     } catch (err) {
       // Handle error
     }
     return port;
   }
-  static get url {
-    Uri url;
+
+  static bool get https {
+    return dotenv.env['API_HTTPS']?.toLowerCase() == 'true';
+  }
+
+  static Uri get url {
+    Uri url = Uri.parse(DefaultApi.url);
+
     try {
-      String blankUrl = ':: blank URL ::';
-      url = Uri.parse(dotenv.env['API_URL'] ?? blankUrl);
+      url = Uri.parse(dotenv.env['API_URL'] ?? '::not url::');
     } catch (err) {
-      url = Uri.parse('https://example.com');
+      url = Uri.parse('${https ? 'https' : 'http'}://$host:$port');
     }
-    url.replace(host: host, port: port);
+    
     return url;
   }
 }
 
-
+class ApiRoutes {
+  static final aprendiz = Router('/aprendiz');
+  static final programa = Router('/programa');
+}
